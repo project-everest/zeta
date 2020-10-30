eafd2e3dc4eabada51387a8c8bfed3b656828702
Tahina Ramananandro <taramana@microsoft.com>
2020-10-30 11:12:47 -0700

    point PATH to package bin to access z3

--- experiments/formats/everparse/make_from_everparse_release.sh
+++ experiments/formats/everparse/make_from_everparse_release.sh
@@ -27,6 +27,8 @@ if [[ -z "$QD_HOME" ]] ; then
     # only work with the F* and KReMLin binaries included in that package
     export FSTAR_HOME=$QD_HOME
     export KREMLIN_HOME=$QD_HOME
+    # add the package binary directory to the PATH to locate z3
+    export PATH=$QD_HOME/bin:$PATH
 fi
 
 exec make "$@"
