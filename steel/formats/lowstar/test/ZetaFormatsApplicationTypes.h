815830d5327d2f239035da392fdf8c45bf558c01
Tahina Ramananandro <taramana@microsoft.com>
2022-02-03 17:05:40 -0800

    `#include` to define app types; test

---
+++ steel/formats/lowstar/test/ZetaFormatsApplicationTypes.h
@@ -0,0 +1,11 @@
+// This is a test file to compile ../dist
+
+#ifndef __ZetaFormatsApplicationTypes_H
+#define __ZetaFormatsApplicationTypes_H
+
+typedef char Zeta_Steel_ApplicationTypes_key_type;
+typedef char Zeta_Steel_ApplicationTypes_value_type;
+
+
+#define __ZetaFormatsApplicationTypes_H_DEFINED
+#endif // __ZetaFormatsApplicationTypes_H
