37000b0187bc52f90f67fd72ff12059baede1179
Arvind Arasu <arvinda@alumni.stanford.edu>
2022-05-03 17:42:06 -0700

    Implement (basic) merkle tree

--- host/include/merkle_tree_impl.h
+++ host/include/merkle_tree_impl.h
@@ -8,7 +8,7 @@ namespace Zeta
 {
     struct BaseKeyHash
     {
-        std::size_t operator() (const BaseKey& key) {
+        std::size_t operator() (const BaseKey& key) const {
             assert (key.IsNormalized());
             auto bytes = key.GetPath().Bytes();
             return bytes[0] ^ bytes[1] ^ bytes[2] ^ bytes[3] ^ std::hash<uint16_t>{}(key.GetDepth());
@@ -17,7 +17,7 @@ namespace Zeta
 
     struct BaseKeyEqual
     {
-        bool operator() (const BaseKey& key1, const BaseKey& key2) {
+        bool operator() (const BaseKey& key1, const BaseKey& key2) const {
             assert(key1.IsNormalized() && key2.IsNormalized());
             return key1.GetPath() == key2.GetPath() &&
                 key1.GetDepth() == key2.GetDepth();
@@ -31,7 +31,7 @@ namespace Zeta
         ~MerkleTreeImpl();
 
         MerkleValue* Put(const BaseKey& key);
-        const MerkleTree* Get(const BaseKey& key) const;
+        const MerkleValue* Get(const BaseKey& key) const;
         MerkleValue* Get (const BaseKey& key);
 
     private:
--- host/src/merkle_tree.cpp
+++ host/src/merkle_tree.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <merkle_tree.h>
 #include <merkle_tree_impl.h>
 
@@ -20,4 +21,39 @@ const MerkleValue* MerkleTree::Get(const BaseKey& key) const
 MerkleValue* MerkleTree::Get(const BaseKey& key)
 {
     return pimpl_->Get(key);
+}
+
+MerkleTreeImpl::MerkleTreeImpl() : mt_{} {}
+
+MerkleTreeImpl::~MerkleTreeImpl() {}
+
+MerkleValue* MerkleTreeImpl::Put(const BaseKey& key)
+{
+    assert (Get(key) == nullptr);
+    auto& mv = mt_[key];
+    return &mv;
+}
+
+MerkleValue* MerkleTreeImpl::Get(const BaseKey& key)
+{
+    auto it = mt_.find(key);
+
+    if (it == mt_.end()) {
+        return nullptr;
+    }
+    else {
+        return &(it->second);
+    }
+}
+
+const MerkleValue* MerkleTreeImpl::Get(const BaseKey& key) const
+{
+    auto it = mt_.find(key);
+
+    if (it == mt_.end()) {
+        return nullptr;
+    }
+    else {
+        return &(it->second);
+    }
 }
