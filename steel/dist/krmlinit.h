8898f55a4bcd4b3ccf690e467dafcf8628c88f37
Tahina Ramananandro <taramana@microsoft.com>
2022-09-15 18:46:18 -0700

    dist

---
+++ steel/dist/krmlinit.h
@@ -0,0 +1,22 @@
+
+
+#ifndef __krmlinit_H
+#define __krmlinit_H
+
+#include "krmllib.h"
+
+
+#include "Zeta_Steel_SafeMain.h"
+#include "steel_atomics.h"
+#include "zeta_application.h"
+
+#if defined(__GNUC__) && !(defined(_WIN32) || defined(_WIN64))
+__attribute__ ((visibility ("hidden")))
+#endif
+
+
+void krmlinit_globals();
+
+
+#define __krmlinit_H_DEFINED
+#endif
