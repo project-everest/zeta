ceffab468ac6a063e98e1e971cab7a86476fe257
Nikhil Swamy <nikswamy@users.noreply.github.com>
2022-12-09 19:06:41 +0000

    a little CheckCPU wrapper and updated instrctions

---
+++ steel/dist/hacl-star-8c77ca1d/gcc-compatible/CheckCPU.c
@@ -0,0 +1,12 @@
+#include "EverCrypt_AutoConfig2.h"
+
+int main() {
+  bool has_avx2 = EverCrypt_AutoConfig2_has_avx2();
+  bool has_aesni = EverCrypt_AutoConfig2_has_aesni();
+  printf(">>>>>>>>>>>>>>>>>Has AVX2=%d, Has AES-NI=%d<<<<<<<<<<<<<<<<<<\n", has_avx2, has_aesni);
+}
+
+
+
+    
+/* cc  -fPIC -I. -I /home/nswamy/everest/karamel/include -I /home/nswamy/everest/karamel/krmllib/dist/minimal -Wall -Wextra -Werror -std=c11 -Wno-unused-variable -Wno-unknown-warning-option -Wno-unused-but-set-variable -Wno-unused-function -Wno-unused-parameter -Wno-infinite-recursion -g -fwrapv -D_BSD_SOURCE -D_DEFAULT_SOURCE -fPIC -Wno-unused -Wno-parentheses -Wno-deprecated-declarations -Wno-#warnings -Wno-error=cpp -Wno-cpp -g -std=gnu11 -O3 -c  CheckCPU.exe CheckCPU.c */
