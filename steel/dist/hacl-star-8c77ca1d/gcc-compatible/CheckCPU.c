81027b4d4364d9e69a8eb49540b3154977e3c9bd
Nikhil Swamy <nikswamy@users.noreply.github.com>
2022-12-13 06:34:48 +0000

    patch to CheckCPU; add a wc target to makefile

--- steel/dist/hacl-star-8c77ca1d/gcc-compatible/CheckCPU.c
+++ steel/dist/hacl-star-8c77ca1d/gcc-compatible/CheckCPU.c
@@ -1,6 +1,7 @@
 #include "EverCrypt_AutoConfig2.h"
 
 int main() {
+  EverCrypt_AutoConfig2_init();
   bool has_avx2 = EverCrypt_AutoConfig2_has_avx2();
   bool has_aesni = EverCrypt_AutoConfig2_has_aesni();
   printf(">>>>>>>>>>>>>>>>>Has AVX2=%d, Has AES-NI=%d<<<<<<<<<<<<<<<<<<\n", has_avx2, has_aesni);
