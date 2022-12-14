da87ec801e35e6d82aeeb428c946adc3d2ad2580
Nikhil Swamy <nikswamy@users.noreply.github.com>
2022-12-14 01:00:57 +0000

    more checks in checkcpu

--- steel/dist/hacl-star-8c77ca1d/gcc-compatible/CheckCPU.c
+++ steel/dist/hacl-star-8c77ca1d/gcc-compatible/CheckCPU.c
@@ -3,8 +3,19 @@
 int main() {
   EverCrypt_AutoConfig2_init();
   bool has_avx2 = EverCrypt_AutoConfig2_has_avx2();
+  bool has_pclmulqdq = EverCrypt_AutoConfig2_has_pclmulqdq();
+  bool has_avx = EverCrypt_AutoConfig2_has_avx();
+  bool has_sse = EverCrypt_AutoConfig2_has_sse();
+  bool has_movbe = EverCrypt_AutoConfig2_has_movbe();
   bool has_aesni = EverCrypt_AutoConfig2_has_aesni();
-  printf(">>>>>>>>>>>>>>>>>Has AVX2=%d, Has AES-NI=%d<<<<<<<<<<<<<<<<<<\n", has_avx2, has_aesni);
+  
+  printf(">>>>>>>>>>>>>>>>>avx2=%d, pclmuldq=%d, avx=%d, sse=%d, movbe=%d, aesni=%d\n",
+         has_avx2,
+         has_pclmulqdq,
+         has_avx,
+         has_sse,
+         has_movbe,
+         has_aesni);
 }
 
 
