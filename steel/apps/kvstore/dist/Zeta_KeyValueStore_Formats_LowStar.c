afc9452689e196febd7c2d6016e6b9039c0f85e9
Tahina Ramananandro <taramana@microsoft.com>
2023-10-25 16:50:29 -0700

    kvstore formats snap

--- steel/apps/kvstore/dist/Zeta_KeyValueStore_Formats_LowStar.c
+++ steel/apps/kvstore/dist/Zeta_KeyValueStore_Formats_LowStar.c
@@ -2,14 +2,14 @@
 
 #include "Zeta_KeyValueStore_Formats_LowStar.h"
 
-#define VALIDATOR_MAX_LENGTH ((uint64_t)4294967295U)
+#define VALIDATOR_MAX_LENGTH (4294967295ULL)
 
 static inline bool is_error(uint64_t positionOrError)
 {
   return positionOrError > VALIDATOR_MAX_LENGTH;
 }
 
-#define VALIDATOR_ERROR_NOT_ENOUGH_DATA ((uint64_t)8589934592U)
+#define VALIDATOR_ERROR_NOT_ENOUGH_DATA (8589934592ULL)
 
 typedef struct __uint64_t_uint64_t_s
 {
@@ -60,12 +60,13 @@ synth_vput_args(__uint64_t___uint64_t_uint16_t uu___)
 FStar_Pervasives_Native_option__uint64_t___uint32_t
 kvstore_key_parser(uint32_t len, uint32_t offset, uint32_t slice_len, uint8_t *a)
 {
+  KRML_MAYBE_UNUSED_VAR(len);
   uint8_t *a_ = a + offset;
   uint64_t is_err;
-  if ((uint64_t)slice_len - (uint64_t)0U < (uint64_t)8U)
+  if ((uint64_t)slice_len - 0ULL < 8ULL)
     is_err = VALIDATOR_ERROR_NOT_ENOUGH_DATA;
   else
-    is_err = (uint64_t)8U;
+    is_err = 8ULL;
   if (is_error(is_err))
     return
       ((FStar_Pervasives_Native_option__uint64_t___uint32_t){ .tag = FStar_Pervasives_Native_None });
@@ -84,20 +85,22 @@ kvstore_key_parser(uint32_t len, uint32_t offset, uint32_t slice_len, uint8_t *a
 
 uint32_t kvstore_key_serializer(uint32_t len, uint32_t offset, uint8_t *a, uint64_t v)
 {
+  KRML_MAYBE_UNUSED_VAR(len);
   uint8_t *a_ = a + offset;
   store64_be(a_, v);
-  return (uint32_t)8U;
+  return 8U;
 }
 
 FStar_Pervasives_Native_option__uint64_t___uint32_t
 kvstore_value_parser(uint32_t len, uint32_t offset, uint32_t slice_len, uint8_t *a)
 {
+  KRML_MAYBE_UNUSED_VAR(len);
   uint8_t *a_ = a + offset;
   uint64_t is_err;
-  if ((uint64_t)slice_len - (uint64_t)0U < (uint64_t)8U)
+  if ((uint64_t)slice_len - 0ULL < 8ULL)
     is_err = VALIDATOR_ERROR_NOT_ENOUGH_DATA;
   else
-    is_err = (uint64_t)8U;
+    is_err = 8ULL;
   if (is_error(is_err))
     return
       ((FStar_Pervasives_Native_option__uint64_t___uint32_t){ .tag = FStar_Pervasives_Native_None });
@@ -116,34 +119,36 @@ kvstore_value_parser(uint32_t len, uint32_t offset, uint32_t slice_len, uint8_t 
 
 uint32_t kvstore_value_serializer(uint32_t len, uint32_t offset, uint8_t *a, uint64_t v)
 {
+  KRML_MAYBE_UNUSED_VAR(len);
   uint8_t *a_ = a + offset;
   store64_be(a_, v);
-  return (uint32_t)8U;
+  return 8U;
 }
 
 FStar_Pervasives_Native_option__Zeta_KeyValueStore_Formats_Types_vget_args_t___uint32_t
 kvstore_vget_args_parser(uint32_t len, uint32_t offset, uint32_t slice_len, uint8_t *a)
 {
+  KRML_MAYBE_UNUSED_VAR(len);
   uint8_t *a_ = a + offset;
   uint64_t pos10;
-  if ((uint64_t)slice_len - (uint64_t)0U < (uint64_t)8U)
+  if ((uint64_t)slice_len - 0ULL < 8ULL)
     pos10 = VALIDATOR_ERROR_NOT_ENOUGH_DATA;
   else
-    pos10 = (uint64_t)8U;
+    pos10 = 8ULL;
   uint64_t pos1;
   if (is_error(pos10))
     pos1 = pos10;
-  else if ((uint64_t)slice_len - pos10 < (uint64_t)8U)
+  else if ((uint64_t)slice_len - pos10 < 8ULL)
     pos1 = VALIDATOR_ERROR_NOT_ENOUGH_DATA;
   else
-    pos1 = pos10 + (uint64_t)8U;
+    pos1 = pos10 + 8ULL;
   uint64_t is_err;
   if (is_error(pos1))
     is_err = pos1;
-  else if ((uint64_t)slice_len - pos1 < (uint64_t)2U)
+  else if ((uint64_t)slice_len - pos1 < 2ULL)
     is_err = VALIDATOR_ERROR_NOT_ENOUGH_DATA;
   else
-    is_err = pos1 + (uint64_t)2U;
+    is_err = pos1 + 2ULL;
   if (is_error(is_err))
     return
       (
@@ -154,10 +159,10 @@ kvstore_vget_args_parser(uint32_t len, uint32_t offset, uint32_t slice_len, uint
   else
   {
     uint64_t x1 = load64_be(a_);
-    uint32_t pos20 = (uint32_t)8U;
+    uint32_t pos20 = 8U;
     uint64_t x2 = load64_be(a_ + pos20);
     __uint64_t_uint64_t x10 = { .fst = x1, .snd = x2 };
-    uint32_t pos2 = (uint32_t)8U + (uint32_t)8U;
+    uint32_t pos2 = 8U + 8U;
     uint16_t x20 = load16_be(a_ + pos2);
     __uint64_t___uint64_t_uint16_t res = { .fst = x10, .snd = x20 };
     Zeta_KeyValueStore_Formats_Types_vget_args_t res0 = synth_vget_args(res);
@@ -174,26 +179,27 @@ kvstore_vget_args_parser(uint32_t len, uint32_t offset, uint32_t slice_len, uint
 FStar_Pervasives_Native_option__Zeta_KeyValueStore_Formats_Types_vput_args_t___uint32_t
 kvstore_vput_args_parser(uint32_t len, uint32_t offset, uint32_t slice_len, uint8_t *a)
 {
+  KRML_MAYBE_UNUSED_VAR(len);
   uint8_t *a_ = a + offset;
   uint64_t pos10;
-  if ((uint64_t)slice_len - (uint64_t)0U < (uint64_t)8U)
+  if ((uint64_t)slice_len - 0ULL < 8ULL)
     pos10 = VALIDATOR_ERROR_NOT_ENOUGH_DATA;
   else
-    pos10 = (uint64_t)8U;
+    pos10 = 8ULL;
   uint64_t pos1;
   if (is_error(pos10))
     pos1 = pos10;
-  else if ((uint64_t)slice_len - pos10 < (uint64_t)8U)
+  else if ((uint64_t)slice_len - pos10 < 8ULL)
     pos1 = VALIDATOR_ERROR_NOT_ENOUGH_DATA;
   else
-    pos1 = pos10 + (uint64_t)8U;
+    pos1 = pos10 + 8ULL;
   uint64_t is_err;
   if (is_error(pos1))
     is_err = pos1;
-  else if ((uint64_t)slice_len - pos1 < (uint64_t)2U)
+  else if ((uint64_t)slice_len - pos1 < 2ULL)
     is_err = VALIDATOR_ERROR_NOT_ENOUGH_DATA;
   else
-    is_err = pos1 + (uint64_t)2U;
+    is_err = pos1 + 2ULL;
   if (is_error(is_err))
     return
       (
@@ -204,10 +210,10 @@ kvstore_vput_args_parser(uint32_t len, uint32_t offset, uint32_t slice_len, uint
   else
   {
     uint64_t x1 = load64_be(a_);
-    uint32_t pos20 = (uint32_t)8U;
+    uint32_t pos20 = 8U;
     uint64_t x2 = load64_be(a_ + pos20);
     __uint64_t_uint64_t x10 = { .fst = x1, .snd = x2 };
-    uint32_t pos2 = (uint32_t)8U + (uint32_t)8U;
+    uint32_t pos2 = 8U + 8U;
     uint16_t x20 = load16_be(a_ + pos2);
     __uint64_t___uint64_t_uint16_t res = { .fst = x10, .snd = x20 };
     Zeta_KeyValueStore_Formats_Types_vput_args_t res0 = synth_vput_args(res);
